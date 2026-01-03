5dae8508f30749119d225c64c8bf0aeca01b2e9b
Dhritiman <dhritimankashyap20@gmail.com>
2026-01-03 22:04:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,38 @@
-const CACHE_NAME = 'study-tracker-v2';
-
+const CACHE_NAME = 'study-tracker-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/index.html'
 ];
 
-// Install
+// Install service worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
-// Activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-// Fetch
+// Fetch from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(
-      cached => cached || fetch(event.request)
-    )
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
+});
+
+// Update service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
