545a1f54d29bd07a1acf027344f9fb35c1510e33
Dhritiman <dhritimankashyap20@gmail.com>
2026-01-03 22:06:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,10 @@
 const CACHE_NAME = 'study-tracker-v1';
 const urlsToCache = [
   '/',
-  '/index.html'
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 // Install service worker
